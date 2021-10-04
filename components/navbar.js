2716d8ebafc7fe77901246f8770603c7b8f44a6d
ananthunalloor <ananthunalloor@gmail.com>
2021-10-04 13:52:38 +0530

    first change

---
+++ components/navbar.js
@@ -0,0 +1,11 @@
+
+export default function Home() {
+    return (
+        <section className="w-full sm:h-16 md:h-20 h-14 top-0 z-20 bg-background">
+            <nav className="h-full flex items-center px-7 md:px-20 xl:max-w-7xl xl:mx-auto">
+                <div className="text-3xl max-w-max mx-auto text-off-white"><h1>movie <span className="text-primary">star</span></h1></div>
+            </nav>
+        </section>  
+    )
+  }
+  
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,14 @@ module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        'background': '#222831',
+        'off-dark': '#393E46',
+        'primary': '#FFD369',
+        'off-white': '#EEEEEE',
+      },
+    },
   },
   variants: {
     extend: {},
